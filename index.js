8f9a7528352665415df2cb51a6b857c605933b1e
Taylor Ng <taylormng95@gmail.com>
2024-05-17 15:07:38 -0400

    completes sending data w fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(userName, userEmail){
+    const postBody = {
+        name: userName,
+        email: userEmail
+    }
+    
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(postBody)
+    })
+    .then(response => response.json())
+    .then(data => document.body.append(data.id))
+    .catch(error => document.body.append(error.message))
+}
